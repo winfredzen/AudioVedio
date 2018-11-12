95b83134868e5af8ee08cd2819aa883e9215707b
wangzhen <wangzhen@curefun.com>
2018-11-12 10:31:00 +0800

    AudioUnit播放PCM

---
+++ 001_AudioUnit播放PCM/001_AudioUnit播放PCM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  001_AudioUnit播放PCM
+//
+//  Created by 王振 on 2018/11/12.
+//  Copyright © 2018 wz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 001_AudioUnit播放PCM/001_AudioUnit播放PCM/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  001_AudioUnit播放PCM
+//
+//  Created by 王振 on 2018/11/12.
+//  Copyright © 2018 wz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
